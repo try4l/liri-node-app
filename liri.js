9c0c4fa5f3056243b63b1c833e3c8fac5e3a6881
Alan Davison <abdavison@cox.net>
2017-10-11 15:18:13 -0700

    Basic functionality for tweets, spotify, movies, file input and logging (with clear)

--- liri.js
+++ liri.js
@@ -23,17 +23,48 @@ var twitter = new Twitter({
   access_token_secret: keys.twitterKeys.access_token_secret
 });
 
-var cmdLineArgs = process.argv;
-console.log("Command Line Args: ", process.argv);
+// Song, Movie and Tweet constructors
+var Movie = function (title, year, imdbRating, rottenTomatoesRating, country, language, plot, actors) {
+	this.title = title;
+	this.year = year;
+	this.imdbRating = imdbRating;
+	this.rottenTomatoesRating = rottenTomatoesRating;
+	this.country = country;
+	this.language = language;
+	this.plot = plot;
+	this.actors = actors;
+};
 
-var command = process.argv[2];
-var item = process.argv[3];
-console.log("argv[0] and argv[1] had better be 'node' and 'liri' (or 'liri.js'), respectively");
-console.log("process.argv[0]: ", process.argv[0]);
-console.log("process.argv[1]: ", process.argv[1]);
-console.log("process.argv[2]: 'command' ", command);
-console.log("process.argv[3]: 'item' ", item);
+Movie.prototype.display = function () {
+	console.log("Movie Title: ", this.title);
+	console.log("Year: ", this.year);
+	console.log("IMDB Rating: ", this.imdbRating);
+	console.log("Rotten Tomatoes Rating: ", this.rottenTomatoesRating);
+	console.log("Country: ", this.country);
+	console.log("Language: ", this.language);
+	console.log("Plot: ", this.plot);
+	console.log("Actors: ", this.actors);
+};
 
+var Song = function (artist, song, previewLink, album) {
+	this.artist = artist;
+	this.song = song;
+	this.album = album;
+	this.previewLink = previewLink;
+};
+
+Song.prototype.display = function () {
+	console.log("Artist: ", this.artist);
+	console.log("Song: ", this.song);
+	console.log("Album: ", this.album);
+	console.log("Preview Link: ", this.previewLink);
+};
+
+// ****************************************************************************
+// LIRI is Language Interpretation and Recognition Interface
+// This is the main object for wrapping all of our LIRI functionality
+// LIRI is a command line node app that takes in parameters and gives back data
+// ****************************************************************************
 var liriObj = {
 
 	processTweets: function () {
@@ -42,39 +73,119 @@ var liriObj = {
 		var params = {screen_name: 'Al_n_Dev'};
 		twitter.get('statuses/user_timeline', params, function(error, tweets, response) {
   			if (!error) {
-    			console.log(tweets);
+  				console.log("--------------------------");
+  				console.log("Showing Tweets for: ", tweets[0].user.name);
+  				console.log("Screen Name: ", tweets[0].user.screen_name);
+  				console.log("--------------------------");
+    			//console.log(tweets);
+    			//console.log(response);
+    			for (var i = 0; i < tweets.length; i++) {
+    				console.log(tweets[i].text + "\n");
+    			}
+
   			}
 		});
 	},
 
-	processSpotify: function () {
+	processSpotify: function (item="The Sign Ace of Base") {
 		console.log("liriObj.processSpotify: ");
+		console.log("item: ", item);
 		// Construct query and show the results
-		spotify
-		  .request('https://api.spotify.com/v1/tracks/7yCPwWs66K8Ba5lFuU2bcx')
-		  .then(function(data) {
-		    console.log(data); 
-		  })
-		  .catch(function(err) {
-		    console.error('Error occurred: ' + err); 
-		  });
+		spotify.search({ type: 'track', query: item, limit: 1 }, function(err, data) {
+  			if (err) {
+    			return console.log('Error occurred: ' + err);
+  			}
+ 
+			console.log(data);
+			console.log(data.tracks.items[0].artists[0].name);
+			console.log(data.tracks.items[0].name); 
+			console.log(data.tracks.items[0].album.name);
+			console.log(data.tracks.items[0].href);
+		});		
+		// spotify
+		//   .request('https://api.spotify.com/v1/tracks/7yCPwWs66K8Ba5lFuU2bcx')
+		//   .then(function(data) {
+		//     console.log(data); 
+		//   })
+		//   .catch(function(err) {
+		//     console.error('Error occurred: ' + err); 
+		//   });
 	},
 
-	processOmdb: function () {
+	processOmdb: function (item="Mr. Nobody") {
 		console.log("liriObj.processOmdb: ");
+		console.log("item: ", item);
 		// construct query and show the results
 		request('http://www.omdbapi.com/?t=dark+star&y=&plot=short&apikey=40e9cece', function (error, response, body) {
   		console.log('error:', error); // Print the error if one occurred
   		console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  		console.log('body:', body); 
+  		console.log('body:', JSON.parse(body)); 
+  		var Body = JSON.parse(body);
+		console.log("Movie Title: ", Body.Title);
+		console.log("Year: ", Body.Year);
+		console.log("IMDB Rating: ", Body.imdbRating);
+		console.log("Rotten Tomatoes Rating: ", Body.Ratings[0].Value);
+		console.log("Country: ", Body.Country);
+		console.log("Language: ", Body.Language);
+		console.log("Plot: ", Body.Plot);
+		console.log("Actors: ", Body.Actors);
 		});
 	},
 
 	processRandom: function () {
 		console.log("liriObj.processRandom: ");
 		// Read the random.txt file and then... do-what-it-says
+		fs.readFile("random.txt", "utf8", function(error, data) {
+			// If the code experiences any errors it will log the error to the console.
+			if (error) {
+			  return console.log(error);
+			}
+			// show the data
+			console.log(data);
+			
+			// Break down into argumements
+			var dataArr = data.split(",");
+			
+			// Have LIRI process the command arguments read from the file
+			liriObj.processCommand(dataArr[0], dataArr[1]);
+		});
 	},
 
+
+	// Save results of LIRI commands in the log.txt file
+	logResults: function (data) {
+		fs.appendFile("log.txt", data + "\n", function(error) {
+  			// Show any error
+  			if (error) {
+    			console.log(err);
+  			} else {
+    		console.log("Data logged");
+  			}
+		});
+	},
+
+	// Show the contents of the log.txt file
+	// Only clear the file if user types an additional 'clear' command line parameter
+	dumpLog: function (item) {
+		fs.readFile("log.txt", "utf8", function(error, data) {
+  			// Show any error
+  			if (error) {
+    			return console.log(error);
+  			}
+  			console.log(data);
+		});
+		if (item==='clear') {
+			fs.writeFile("log.txt", " -- LIRI Log File -- " + "\n", function(err) {
+  				// Show any error
+  				if (err) {
+    				return console.log(err);
+  				}
+    			console.log("LIRI log file cleared");
+			});
+		}
+	},
+
+	// Tell the user what they can do
 	showUsage: function () {
 		console.log("--------------------------");
 		console.log("Liri Usage: you must type:");
@@ -85,63 +196,90 @@ var liriObj = {
         console.log("\tspotify-this-song 'song name'");
         console.log("\tmovie-this 'movie title'");
         console.log("\tdo-what-it-says");
-        console.log("\tnote: 'spotify-this-song' or 'movie-this' option");
+        console.log("\tdump-log <clear>");
+        console.log("\tnotes: ");
+        console.log("\t      'spotify-this-song' or 'movie-this' option");
         console.log("\t      should be followed by a quoted string (song or movie, resp.) ");
-		console.log("--------------------------");		
+        console.log("\t      <clear> flag on dump-log command is optional and will erase log");
+		console.log("--------------------------");
+
 	},
+
 
 	showKeys: function () {
 		// Print everything in exports.
-		console.log("--------------------------");
-		console.log("EXPORTS");
-		console.log(keys);
-		console.log("--------------------------");
-		// Print exports individually
-		console.log("twitterKeys:");
-		console.log(keys.twitterKeys);
-		console.log("--------------------------");
-		console.log("spotifyKeys:");
-		console.log(keys.spotifyKeys);
-		console.log("--------------------------");
+		// console.log("--------------------------");
+		// console.log("EXPORTS");
+		// console.log(keys);
+		// console.log("--------------------------");
+		// // Print exports individually
+		// console.log("twitterKeys:");
+		// console.log(keys.twitterKeys);
+		// console.log("--------------------------");
+		// console.log("spotifyKeys:");
+		// console.log(keys.spotifyKeys);
+		// console.log("--------------------------");
 		console.log("omdbKeys:");
 		console.log(keys.omdbKeys);
+		this.logResults(keys.omdbKeys);
 		// Show the client api objects
-		console.log("Spotity and Twitter client vars:");
-		console.log("Spotify: ", spotify);
-		console.log("Twitter: ", twitter);
-		console.log("request: ", request);
-		console.log("fs: ", fs);
+		// console.log("Spotity and Twitter client vars:");
+		// console.log("Spotify: ", spotify);
+		// console.log("Twitter: ", twitter);
+		// console.log("request: ", request);
+		// console.log("fs: ", fs);
 	},
+
+	// Parse the command line arguments
+	processCmdLine: function (cmdLineArgs) {
+		var cmdLineArgs = process.argv;
+		console.log("Command Line Args: ", process.argv);
+
+		var command = process.argv[2];
+		var item = process.argv[3];
+
+		this.processCommand(command, item);
+	},
+
+	// Look up the LIRI command for processing
+	processCommand: function (command, item) {
+	// Make it so liri.js can take in one of the following commands:
+	// 		my-tweets
+	// 		spotify-this-song
+	// 		movie-this
+	// 		do-what-it-says
+		switch(command) {
+    		case 'my-tweets':
+    		    this.processTweets();
+    		    break;
+		
+    		case 'spotify-this-song':
+    		    this.processSpotify(item);
+    		    break;
+		
+    		case 'movie-this':
+    		    this.processOmdb(item);
+    		    break;
+		
+    		case 'do-what-it-says':
+    		    this.processRandom();
+    		    break;
+		
+    		case 'show-keys':
+    		    this.showKeys();
+    		    break;
+
+    		case 'dump-log':
+    			this.dumpLog(item);
+    			break;
+		
+    		default:
+    		    this.showUsage();
+    		    break;
+		};
+	},
+
 } // liriObj
 
-// Make it so liri.js can take in one of the following commands:
-// 	my-tweets
-// 	spotify-this-song
-// 	movie-this
-// 	do-what-it-says
-switch(command) {
-    case 'my-tweets':
-        liriObj.processTweets();
-        break;
-
-    case 'spotify-this-song':
-        liriObj.processSpotify();
-        break;
-
-    case 'movie-this':
-        liriObj.processOmdb();
-        break;
-
-    case 'do-what-it-says':
-        liriObj.processRandom();
-        break;
-
-    case 'show-keys':
-        liriObj.showKeys();
-        break;
-
-    default:
-        liriObj.showUsage();
-        break;
-};
-
+// Use the LIRI Object to fullful user requests
+liriObj.processCmdLine();
